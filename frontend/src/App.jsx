a1b52ef74377725a97d669cfa6fbf5f7a16f7ddc
Nick G <nolimit48@yandex.ru>
2025-03-08 14:13:54 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
 //import './index.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import LoginForm from "./pages/loginPage.jsx";
 import RegistrationForm from "./pages/registerPage.jsx";
 
 function App() {
-    return (
-        <div className="auth-container">
-            <div className="card">
-                <LoginForm/>
-            </div>
-        </div>
-    );
+    return (<Router>
+        <nav>
+            <Link to="/">LoginForm</Link>
+            <span></span>
+            <Link to="/login">RegistrationForm</Link>
+        </nav>
+
+        <Routes>
+            <Route path="/" element={<div className="auth-container">
+                <div className="card">
+                    <LoginForm/>
+                </div>
+            </div>}/>
+
+            <Route path="/login" element={<div className="auth-container">
+                <div className="card">
+                    <RegistrationForm/>
+                </div>
+            </div>}/>
+        </Routes>
+    </Router>);
 }
+
 
 export default App;
