12fb2c2d2e31c7f49648fbf0a852be5a4df0972b
Nick G <nolimit48@yandex.ru>
2025-03-10 15:58:55 +0100

    update

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,32 +1,53 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import {Formik, Field, Form} from 'formik';
 import StyledWrapper from "../components/AuthStyles.js";
 import {Link} from 'react-router-dom';
 
+
 const LoginPage = () => {
+    const navigate = useNavigate();
+
     return (<StyledWrapper>
         <div className="login-box">
             <p>Login</p>
-            <form>
-                <div className="user-box">
-                    <input required name type="text"/>
-                    <label>Name</label>
-                </div>
-                <div className="user-box">
-                    <input required name type="password"/>
-                    <label>Password</label>
-                </div>
-                <a href="#">
-                    <span/>
-                    <span/>
-                    <span/>
-                    <span/>
-                    Submit
-                </a>
-            </form>
+            <Formik
+                initialValues={{name: '', password: ''}}
+
+                onSubmit={async (values) => {
+                    await new Promise((r) => setTimeout(r, 500));
+                    alert(JSON.stringify(values, null, 2));
+                    navigate('/');
+                }}>
+
+                <Form>
+                    <div className="user-box">
+                        <Field id="name" name="name" required/>
+                        <label htmlFor="name">Name</label>
+                    </div>
+
+                    <div className="user-box">
+                        <Field id="password" name="password" type="password" required/>
+                        <label htmlFor="password">Password</label>
+                    </div>
+
+                    <button type="submit" className="btn">
+                        <span/>
+                        <span/>
+                        <span/>
+                        <span/>
+                        Submit
+                    </button>
+
+
+                </Form>
+            </Formik>
+
             <p className="no-wrap">
                 Don't have an account?
                 <Link to="/register" style={{marginLeft: '122px'}}>Sign up!</Link>
             </p>
+
         </div>
     </StyledWrapper>)
 }
