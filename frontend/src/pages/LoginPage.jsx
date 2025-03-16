a9a7b0ef526133c05763aba35c83ac35a04de43c
Nick G <nolimit48@yandex.ru>
2025-03-16 22:19:39 +0100

    update

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import AuthForm from '../components/auth/AuthForm.jsx';
+import { useDispatch } from 'react-redux';
 import { loginUser } from '../api/authApi.js';
+import AuthForm from '../components/auth/AuthForm.jsx';
 
-const LoginPage = () => (
-  <div>
-    <AuthForm
-      title="Login"
-      fields={[
-        { name: 'name', label: 'Name' },
-        { name: 'password', label: 'Password', type: 'password' },
-      ]}
-      initialValues={{ name: '', password: '' }}
-      onSubmit={loginUser}
-      redirectPrompt="Don't have an account?"
-      redirectName="Sign up!"
-      switchLink="/register"
-    />
-  </div>
-);
+const LoginPage = () => {
+  const dispatch = useDispatch();
+
+  const handleLogin = async (values) => {
+    await dispatch(loginUser(values));
+  };
+
+  return (
+    <div>
+      <AuthForm
+        title="Login"
+        fields={[
+          { name: 'name', label: 'Name' },
+          { name: 'password', label: 'Password', type: 'password' },
+        ]}
+        initialValues={{ name: '', password: '' }}
+        onSubmit={handleLogin}
+        redirectPrompt="Don't have an account?"
+        redirectName="Sign up!"
+        switchLink="/register"
+      />
+    </div>
+  );
+};
 
 export default LoginPage;
