1bfba1d0ea3b0e0b376011a21a2d9835d712a3be
Nick G <nolimit48@yandex.ru>
2025-04-04 15:31:29 +0200

    update

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,32 +5,33 @@ import pluginPrettier from 'eslint-plugin-prettier';
 import configPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
-export default [{
-  files: ['**/*.{js,mjs,cjs,jsx}'], languageOptions: {
-    globals: {
-      ...globals.browser
-    }
-  }
-},
-
-  pluginJs.configs.recommended,
-
-
-  pluginReact.configs.flat.recommended,
-
-
+export default [
   {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
     plugins: {
-      prettier: pluginPrettier
-    }, rules: {
-
-      ...pluginPrettier.configs.recommended.rules
-    }
-  },
-
-
-  {
+      react: pluginReact,
+      prettier: pluginPrettier,
+    },
     rules: {
-      ...configPrettier.rules
-    }
-  }];
+      ...pluginJs.configs.recommended.rules, // Базовые правила JavaScript
+      ...pluginReact.configs.recommended.rules, // Рекомендованные правила React
+      ...configPrettier.rules, // Отключаем конфликты с Prettier
+      'prettier/prettier': 'error', // Форматирование через Prettier
+    },
+    settings: {
+      react: {
+        version: 'detect', // Автоматически определяет версию React
+      },
+    },
+  },
+];
