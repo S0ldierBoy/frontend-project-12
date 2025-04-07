97592109eb161fd43d867cc840244d04bdc7d36d
Nick G <nolimit48@yandex.ru>
2025-04-07 18:02:19 +0200

    update

--- frontend/src/utils/validation/authSchema.js
+++ frontend/src/utils/validation/authSchema.js
@@ -5,14 +5,14 @@ const t = i18n.t;
 
 export const loginSchema = Yup.object({
   name: Yup.string()
-    .min(3, t('validation.auth.tooLong'))
-    .max(20, t('validation.auth.tooLong'))
-    .required(t('validation.auth.required'))
+    // .min(3, t('validation.auth.tooLong'))
+    // .max(20, t('validation.auth.tooLong'))
+    // .required(t('validation.auth.required'))
     .label(t('auth.NickName.name')),
 
   password: Yup.string()
-    .min(6, t('validation.auth.tooShort'))
-    .required(t('validation.auth.required'))
+    // .min(6, t('validation.auth.tooShort'))
+    // .required(t('validation.auth.required'))
     .label(t('auth.formField.password')),
 });
 
