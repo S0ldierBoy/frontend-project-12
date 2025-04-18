72200dda4a2522b0d598ab7605ad81a952eb2aa4
Nick G <nolimit48@yandex.ru>
2025-04-18 14:41:20 +0200

    update

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -1,22 +1,28 @@
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { I18nextProvider } from 'react-i18next';
-import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import { Provider as ReduxProvider } from 'react-redux';
 import { StrictMode } from 'react';
 import store from './app/store.js';
 import App from './app/App.jsx';
 import './index.css';
 import i18n from './app/i18n.js';
 import leoProfanity from 'leo-profanity';
+import rollbarConfig from './utils/rollbarConfig.js';
 
 leoProfanity.loadDictionary(['ru', 'en']);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <ReduxProvider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <App />
+          </I18nextProvider>
+        </ReduxProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   </StrictMode>
 );
--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { Provider, ErrorBoundary } from '@rollbar/react';
 import routes from './routes.jsx';
 import { initSocketListeners } from '../services/socket/listeners.js';
 import ToastNotifications from '../components/ui/ToastNotifications.jsx';
-import rollbarConfig from '../utils/rollbarConfig.js';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,16 +14,12 @@ const App = () => {
 
   return (
     <Router>
-      <Provider config={rollbarConfig}>
-        <ErrorBoundary>
-          <ToastNotifications />
-          <Routes>
-            {routes.map(({ path, element }) => (
-              <Route key={path} path={path} element={element} />
-            ))}
-          </Routes>
-        </ErrorBoundary>
-      </Provider>
+      <ToastNotifications />
+      <Routes>
+        {routes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Routes>
     </Router>
   );
 };
