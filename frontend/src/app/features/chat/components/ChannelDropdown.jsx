d4bfcc0e31ae9ab86ddb6d522145fd81221bd81f
Nick G <nolimit48@yandex.ru>
2025-04-21 21:11:06 +0200

    update

--- frontend/src/app/features/chat/components/ChannelDropdown.jsx
+++ frontend/src/app/features/chat/components/ChannelDropdown.jsx
@@ -1,19 +1,38 @@
+import { useState } from 'react';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
-import RemoveChannelDropdownItem from './RemoveChannelDropdownItem.jsx';
-import RenameChannelDropdownItem from './RenameChannelDropdownItem.jsx';
+import RenameChannelModal from './RenameChannelModal.jsx';
+import RemoveChannelModal from './RemoveChannelModal.jsx';
 
-const ChannelDropdown = ({ channelId }) => (
-  <Dropdown as={ButtonGroup} className="dropdown">
-    <Dropdown.Toggle split variant="success">
-      <span className="sr-only">Управление каналом</span>
-    </Dropdown.Toggle>
+const ChannelDropdown = ({ channelId }) => {
+  const [renameOpen, setRenameOpen] = useState(false);
+  const [removeOpen, setRemoveOpen] = useState(false);
 
-    <Dropdown.Menu>
-      <RemoveChannelDropdownItem channelId={channelId} />
-      <RenameChannelDropdownItem channelId={channelId} />
-    </Dropdown.Menu>
-  </Dropdown>
-);
+  const handleSelect = (key) => {
+    if (key === 'rename') setRenameOpen(true);
+    if (key === 'remove') setRemoveOpen(true);
+  };
+
+  return (
+    <>
+      <Dropdown as={ButtonGroup} className="dropdown" onSelect={handleSelect}>
+        <Dropdown.Toggle split variant="success" />
+
+        <Dropdown.Menu>
+          <Dropdown.Item eventKey="rename">Переименовать</Dropdown.Item>
+          <Dropdown.Item eventKey="remove">Удалить</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      {renameOpen && (
+        <RenameChannelModal channelId={channelId} onClose={() => setRenameOpen(false)} />
+      )}
+
+      {removeOpen && (
+        <RemoveChannelModal channelId={channelId} onClose={() => setRemoveOpen(false)} />
+      )}
+    </>
+  );
+};
 
 export default ChannelDropdown;
