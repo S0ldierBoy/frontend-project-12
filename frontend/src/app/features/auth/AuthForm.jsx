08e1c1b160b38846bf4a4ae9e8766dfc0718f67f
Nick G <nolimit48@yandex.ru>
2025-04-24 19:59:37 +0300

    update

--- frontend/src/app/features/auth/AuthForm.jsx
+++ frontend/src/app/features/auth/AuthForm.jsx
@@ -1,7 +1,6 @@
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import {
-  Formik, Field, Form, ErrorMessage,
-} from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import StyledWrapper from '../../../styles/authFormStyles.js';
 import { ROUTES } from '../../routes.jsx';
 
